286b06fa9299958e0d1c1da1592c30da0518f7c8
chenmeng <meng.chen@southgis.com>
2020-12-21 14:58:30 +0800

    初始化仓库

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Im/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Im from './components/Im.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Im
   }
 }
 </script>
